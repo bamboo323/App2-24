215c5049254dfbbe3ee449d6d6979c0d6f07b682
EC2 Default User <ec2-user@ip-172-31-4-152.ec2.internal>
2022-05-17 07:15:35 +0000

    [Add]ページネーション 2-22

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,10 @@ has_one_attached :image
 
 belongs_to :user
 has_many :post_comments, dependent: :destroy
+has_many :favorites, dependent: :destroy
+
+validates :shop_name, presence: true
+validates :image, presence: true
 
   def get_image
     unless image.attached?
@@ -12,4 +16,10 @@ has_many :post_comments, dependent: :destroy
     end
     image
   end
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
